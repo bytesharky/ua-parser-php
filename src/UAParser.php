343800879f7f10873e8a85ed438e61d452c5496c
Forgot Fish <iBottle@qq.com>
2023-02-23 13:15:41 +0800

    add getip function

--- src/UAParser.php
+++ src/UAParser.php
@@ -65,7 +65,24 @@ class UAParser extends BaseClass{
         return $this->uastring = $uastring;
     }
 
-
+    public function getip() {
+        static $ip = '';
+        $ip = $_SERVER['REMOTE_ADDR'];
+        if(isset($_SERVER['HTTP_CDN_SRC_IP'])) {
+            $ip = $_SERVER['HTTP_CDN_SRC_IP'];
+        } elseif (isset($_SERVER['HTTP_CLIENT_IP']) && preg_match('/^([0-9]{1,3}\.){3}[0-9]{1,3}$/', $_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif(isset($_SERVER['HTTP_X_FORWARDED_FOR']) AND preg_match_all('#\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}#s', $_SERVER['HTTP_X_FORWARDED_FOR'], $matches)) {
+            foreach ($matches[0] AS $xip) {
+                if (!preg_match('#^(10|172\.16|192\.168)\.#', $xip)) {
+                    $ip = $xip;
+                    break;
+                }
+            }
+        }
+        return $ip;
+    }
+    
     // public methods
     public function getBrowser () {
         $browser = new UAItem([NAME, VERSION], [[NAME], '/\s?browser$/i']);
